39ff9d31d5600f6d567bf8750eaa7a0dd0cffa12
Jahidul Pabel Islam <me@jahidulpabelislam.com>
2021-11-10 23:39:00 +0000

    Tidy up string concatenation with variables

--- src/Connection.php
+++ src/Connection.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Simple class wrapped around PDO with convenient methods.
  *
@@ -39,7 +40,7 @@ class Connection {
         $username = $config["username"];
         $password = $config["password"];
 
-        $dsn = "mysql:host={$host};dbname={$database};charset-UTF-8";
+        $dsn = "mysql:host=$host;dbname=$database;charset-UTF-8";
         $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION];
 
         try {
@@ -86,7 +87,7 @@ class Connection {
             if (isset($params)) {
                 $bindings = [];
                 foreach ($params as $key => $value) {
-                    $bindings[":{$key}"] = $value;
+                    $bindings[":$key"] = $value;
                 }
 
                 $stmt = $this->pdo->prepare($query);
@@ -100,7 +101,7 @@ class Connection {
         }
         catch (PDOException $error) {
             throw new Exception(
-                "Error executing query on database: {$error->getMessage()}, using query: {$query} and params: " . print_r($params, true),
+                "Error executing query on database: {$error->getMessage()}, using query: $query and params: " . print_r($params, true),
                 $error->getCode(),
                 $error->getPrevious()
             );
